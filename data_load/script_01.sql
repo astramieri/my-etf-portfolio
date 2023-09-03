c66f9bfdb5df4f3ac4ae344109137ae6bf9e7dfb
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-09-03 16:22:14 +0200

    Added first table (PF_ETF) to data model

---
+++ data_load/script_01.sql
@@ -0,0 +1,11 @@
+INSERT INTO pf_etf (
+    title,
+    isin,
+    ticker
+) VALUES (
+    'iShares Core MSCI World UCITS ETF USD (Acc)',
+    'IE00B4L5Y983',
+    'SWDA'
+);
+
+commit;
--- data_model/pf_etf.sql
+++ data_model/pf_etf.sql
@@ -1,9 +1,35 @@
+drop sequence pf_etf_seq;
+
+create sequence pf_etf_seq nocache;
+
 drop table pf_etf;
 
 create table pf_etf (
-    id number
+    id              number(10)      not null,
+    title           varchar2(100)   not null,
+    isin            varchar2(12)    not null,
+    ticker          varchar2(4)     not null,
+    created         timestamp(3)    not null,
+    updated         timestamp(3)    not null
 );
 
-insert into pf_etf values (1);
+alter table pf_etf
+    modify id default on null pf_etf_seq.nextval;
 
-commit;
+alter table pf_etf
+    add constraint pf_etf_pk primary key (id);
+    
+create or replace trigger pf_etf_trg_01
+    before insert or update 
+    on pf_etf
+    for each row
+declare
+    v_systimestamp timestamp := systimestamp;
+begin
+    if inserting then
+        :new.created := v_systimestamp;
+    end if;
+    
+    :new.updated := v_systimestamp;
+end pf_etf_trg_01;
+/

63afda52736713b40bfc7a79be9d0f5b0d3ce14d
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-09-13 12:10:35 +0200

    Changed icon color

--- apex/pf_app_v.sql
+++ apex/pf_app_v.sql
@@ -6,7 +6,7 @@ CREATE OR REPLACE VIEW pf_app_v AS
             SELECT
                 'Indexes'                                                                                        AS title,
                 q'[Financial market's performance]'                                                              AS subtitle,
-                q'[Statistical measures that show the performances of a market or a specific sector over time.]' AS body,
+                q'[Securities basket representing a whole market or a submarket.]' AS body,
                 'fa fa-bar-chart u-color-1'                                                                      AS icon,
                 apex_page.get_url(p_page => 2)                                                                   AS url,
                 1                                                                                                AS sequence
